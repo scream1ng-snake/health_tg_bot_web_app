ddc72156c3842adffd9736e6fdd79a45d576923f
lykasov_am <lykasov_am@lexema.ru>
2023-06-15 17:47:04 +0500

    UPD: physical activity chart completed

--- src/components/charts/PhysicalActivity/PhysicalActivity.tsx
+++ src/components/charts/PhysicalActivity/PhysicalActivity.tsx
@@ -2,24 +2,53 @@ import React from 'react';
 import './PhysicalActivity.css';
 import { PieChart, Pie, Legend, Tooltip, Cell, ResponsiveContainer } from "recharts";
 import { renderCustomizedLabel } from '../helpers';
+import { http, LastWeek, LastWeekUntilToday, NotEnoughtData, prepareDate } from '../../common';
 
-const data02 = [
-  { name: "Моя актвиность за неделю", value: 150, color: '#0088FE' },
-  { name: "Оставшаяся активность за неделю", value: 150, color: '#FF8042' },
-];
+
+const layout = {
+  width: '80%',
+  height: 300
+}
 
 /**
  * Физическая нагрузка за неделю
+ * физическая нагрузка, цель: 150 минут в неделю (общая продолжительность за неделю). 
+ * Пирожковая диаграмма: 150 минут - 100% 
  * @returns 
  */
 const PhysicalActivity: React.FC = () => {
-  const layout = { 
-    width: '80%',
-    height: 300
-  }
+  const [data, setData] = React.useState<answer[]>([]);
+  /** Недостаточно данных */
+  const isNotEnought = !data.length;
+
+  React.useEffect(() => {
+    http.post('question_2')
+      .then(LastWeek)
+      .then(prepareDate)
+      .then(setData)
+      .catch(console.log)
+  }, [])
+
+  const physicalActivity = data.reduce((acc, cur) => (
+    acc + Number(cur.answer_text)
+  ), 0)
+
+  const data02 = [
+    { 
+      name: "Моя актвиность за неделю", 
+      value: physicalActivity ?? 0, 
+      color: '#0088FE' 
+    },
+    { 
+      name: "Оставшаяся активность за неделю", 
+      value: 150 - physicalActivity ?? 0, 
+      color: '#FF8042' 
+    },
+  ];
   return (
     <div className='responsiveChart'>
-      <h3>Физическая нагрузка</h3>
+      <h3>Физическая нагрузка за неделю</h3>
+      {isNotEnought ? <NotEnoughtData /> : null}
       <ResponsiveContainer {...layout}>
         <PieChart height={400}>
           <Pie
@@ -31,7 +60,7 @@ const PhysicalActivity: React.FC = () => {
             outerRadius={120}
             innerRadius={70}
           >
-            {data02.map((entry, index) => 
+            {data02.map((entry, index) =>
               <Cell key={`cell-${index}`} fill={entry.color} />
             )}
           </Pie>
