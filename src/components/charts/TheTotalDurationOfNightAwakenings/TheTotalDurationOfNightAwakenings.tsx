5a430d8a3e69e6fbe454389d9379490f697ff438
lykasov_am <lykasov_am@lexema.ru>
2023-07-05 16:28:23 +0500

    UPD: total duration of night awakenings chart done

--- src/components/charts/TheTotalDurationOfNightAwakenings/TheTotalDurationOfNightAwakenings.tsx
+++ src/components/charts/TheTotalDurationOfNightAwakenings/TheTotalDurationOfNightAwakenings.tsx
@@ -1,45 +1,14 @@
 import React from 'react';
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, ResponsiveContainer } from 'recharts';
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, ResponsiveContainer, Cell } from 'recharts';
+import { useLoading, useStore } from '../../../hooks';
+import { FailedRequest, http, isDoctorPage, LoaderChart, NotEnoughtData, prepareDate, WithAverage, withoutDuplicates } from '../../common';
 import './TheTotalDurationOfNightAwakenings.css';
 
+const layout = {
+  width: '80%',
+  height: 200
+}
 
-const data: Array<{
-  date: string,
-  nightAwakeningsDuration: number
-}> = [
-    {
-      date: '11.06.2022',
-      nightAwakeningsDuration: 50,
-    },
-    {
-      date: '12.06.2022',
-      nightAwakeningsDuration: 30,
-    },
-    {
-      date: '13.06.2022',
-      nightAwakeningsDuration: 40,
-    },
-    {
-      date: '14.06.2022',
-      nightAwakeningsDuration: 30,
-    },
-    {
-      date: '15.06.2022',
-      nightAwakeningsDuration: 20,
-    },
-    {
-      date: '16.06.2022',
-      nightAwakeningsDuration: 5,
-    },
-    {
-      date: '17.06.2022',
-      nightAwakeningsDuration: 0,
-    },
-    {
-      date: 'средняя',
-      nightAwakeningsDuration: (50 + 30 + 40 + 30 + 20 + 5 + 0) / 7,
-    },
-  ]
 
 /**
  * суммарная длительность ночных пробуждений за ночь, 
@@ -48,25 +17,63 @@ const data: Array<{
  * @returns 
  */
 const TheTotalDurationOfNightAwakenings: React.FC = () => {
-  const layout = {
-    width: '80%',
-    height: 200
+  const [data, setData] = React.useState<answer[]>([]);
+
+  const { currentUser, selectedUser, startDate, endDate } = useStore();
+
+  const { isFailed, isLoad, onStart, onFailed, onSussess } = useLoading();
+
+  /** Недостаточно данных */
+  const isNotEnought = !data.length;
+
+  const fetchData = (userId: string) => {
+    onStart();
+    http.post(userId, 'question_10', startDate, endDate)
+      .then(withoutDuplicates)
+      .then(prepareDate)
+      .then(WithAverage)
+      .then(setData)
+      .then(onSussess)
+      .catch(onFailed)
   }
+
+  React.useEffect(() => {
+    if (isDoctorPage()) {
+      if (selectedUser) fetchData(selectedUser)
+    } else {
+      if (currentUser) fetchData(currentUser)
+    }
+    // eslint-disable-next-line
+  }, [startDate, endDate, currentUser, selectedUser])
   return (
     <div className='responsiveChart'>
-      <h3>суммарная длительность ночных пробуждений за ночь</h3>
+      <h3>Суммарная длительность ночных пробуждений за ночь</h3>
+      {isFailed
+        ? <FailedRequest />
+        : isLoad
+          ? <LoaderChart isLoad={isLoad} />
+          : isNotEnought
+            ? <NotEnoughtData />
+            : null
+      }
       <ResponsiveContainer {...layout}>
         <BarChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis dataKey='nightAwakeningsDuration' />
+          <XAxis dataKey="cdate" />
+          <YAxis dataKey='answer_text' />
           <Tooltip />
           <Legend />
           <Bar
-            name='суммарная длительность пробуждений за ночь'
-            dataKey="nightAwakeningsDuration"
+            name='Суммарная длительность пробуждений за ночь, мин'
+            dataKey="answer_text"
             fill="#4F81BD"
-          />
+          >
+            {
+              data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.cdate === 'Средняя' ? '#4FBDA3' : '#4F81BD'} />
+              ))
+            }
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     </div>
