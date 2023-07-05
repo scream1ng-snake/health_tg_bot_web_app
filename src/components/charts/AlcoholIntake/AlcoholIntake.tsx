442e1103b6f4541c56dc08aa6f155ccb725b79f5
lykasov_am <lykasov_am@lexema.ru>
2023-07-05 13:23:07 +0500

    UPD: alchocol intake chart complete

--- src/components/charts/AlcoholIntake/AlcoholIntake.tsx
+++ src/components/charts/AlcoholIntake/AlcoholIntake.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './AlcoholIntake.css';
 import { PieChart, Pie, Legend, Tooltip, Cell, ResponsiveContainer } from "recharts";
 import { renderCustomizedLabel } from '../helpers';
+import { useLoading, useStore } from '../../../hooks';
+import { FailedRequest, http, isDoctorPage, LoaderChart, NotEnoughtData, prepareDate } from '../../common';
 //import { http } from '../../common';
 
 const layout = {
@@ -14,30 +16,61 @@ const layout = {
  * @returns 
  */
 const AlcoholIntake: React.FC = () => {
-  // const [data, setData] = React.useState<answer[]>([]);
-  // const [data] = React.useState<answer[]>([]);
-  /** Недостаточно данных */
-  // const isNotEnought = !data.length;
+  const [data, setData] = React.useState<answer[]>([]);
 
-  // React.useEffect(() => {
-  //   http.post('question_3') // todo
-  // }, [])
+  const { currentUser, selectedUser, startDate, endDate } = useStore()
+
+  const { isFailed, isLoad, onStart, onFailed, onSussess } = useLoading();
+
+  /** Недостаточно данных */
+  const isNotEnought = !data.length;
+
+  const fetchData = (userId: string) => {
+    onStart();
+    http.post(userId, 'question_4', startDate, endDate)
+      .then(prepareDate)
+      .then(setData)
+      .then(onSussess)
+      .catch(onFailed)
+  }
+
+  React.useEffect(() => {
+    if (isDoctorPage()) {
+      if (selectedUser) fetchData(selectedUser)
+    } else {
+      if (currentUser) fetchData(currentUser)
+    }
+    // eslint-disable-next-line
+  }, [startDate, endDate, currentUser, selectedUser])
+
+  let daysWithAlco = 0;
+  for(const item of data) {
+    if(item.answer_text === 'true') daysWithAlco++
+  }
 
   const data02 = [
-    { 
-      name: "дней приема алкоголя", 
-      value: 3, 
-      color: '#8884d8' 
+    {
+      name: "дней приема алкоголя",
+      value: daysWithAlco,
+      color: '#8884d8'
     },
-    { 
-      name: "дней без приема алкоголя", 
-      value: 4, 
-      color: '#82ca9d' 
+    {
+      name: "дней без приема алкоголя",
+      value: (data.length - daysWithAlco) ?? 7,
+      color: '#82ca9d'
     },
   ];
   return (
     <div className='responsiveChart'>
-      <h3>число дней с приемом алкоголя за неделю (абсолютное значение)</h3>
+      <h3>Число дней с приемом алкоголя за неделю (абсолютное значение)</h3>
+      {isFailed
+        ? <FailedRequest />
+        : isLoad
+          ? <LoaderChart isLoad={isLoad} />
+          : isNotEnought
+            ? <NotEnoughtData />
+            : null
+      }
       <ResponsiveContainer {...layout}>
         <PieChart>
           <Pie
