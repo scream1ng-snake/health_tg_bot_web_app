8fdcca90a9a9b2faf1f72747d4211524467e5936
lykasov_am <lykasov_am@lexema.ru>
2023-07-05 13:25:12 +0500

    FIX: validate date before http request

--- src/components/common/fetch.ts
+++ src/components/common/fetch.ts
@@ -1,30 +1,36 @@
+import moment from "moment";
 import { BOT_URL } from "./config";
 
 export const http = {
   get: () => { },
   post: async function PostData(
-    userid: string, 
-    question: string, 
-    startdate: string, 
+    userid: string,
+    question: string,
+    startdate: string,
     enddate: string
   ) {
-    const response = await fetch(BOT_URL, {
-      method: 'POST',
-      headers: {
-        "Content-Type": 'application/json',
-        "Accept": "application/json",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
+    const isValidStartDate = moment(startdate, 'YYYY-MM-DD', true).isValid();
+    const isValidEndDate = moment(enddate, 'YYYY-MM-DD', true).isValid();
+    if (isValidEndDate && isValidStartDate) {
+      const response = await fetch(BOT_URL, {
+        method: 'POST',
+        headers: {
+          "Content-Type": 'application/json',
+          "Accept": "application/json",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Connection": "keep-alive",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
 
-      body: JSON.stringify({
-        userid,
-        botstate: question,
-        startdate: startdate.replace(/[-_]/g,""),
-        enddate: enddate.replace(/[-_]/g,"")
-      })
-    });
-    return await response.json() as Array<answer>;
-  },
+        body: JSON.stringify({
+          userid,
+          botstate: question,
+          startdate: startdate.replace(/[-_]/g, ""),
+          enddate: enddate.replace(/[-_]/g, "")
+        })
+      });
+      return await response.json() as Array<answer>;
+    }
+    return [];
+  }
 }
