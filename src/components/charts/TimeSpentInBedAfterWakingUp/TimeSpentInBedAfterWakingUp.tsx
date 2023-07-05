d4d21ade9f516b5757bdceae3fb999f79fbb62dd
lykasov_am <lykasov_am@lexema.ru>
2023-07-05 17:19:35 +0500

    UPD: time spent in bed after awake complete

--- src/components/charts/TimeSpentInBedAfterWakingUp/TimeSpentInBedAfterWakingUp.tsx
+++ src/components/charts/TimeSpentInBedAfterWakingUp/TimeSpentInBedAfterWakingUp.tsx
@@ -1,45 +1,26 @@
+import moment from 'moment';
 import React from 'react';
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, ResponsiveContainer } from 'recharts';
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, ResponsiveContainer, Cell } from 'recharts';
+import { useLoading, useStore } from '../../../hooks';
+import { 
+  FailedRequest, 
+  findOneOrFindLast, 
+  http, 
+  isDoctorPage, 
+  LoaderChart, 
+  NotEnoughtData, 
+  prepareDate, 
+  strTimeToNum, 
+  validateHHMMstr, 
+  WithAverageInt, 
+  withoutDuplicates 
+} from '../../common';
 import './TimeSpentInBedAfterWakingUp.css';
 
-
-const data: Array<{
-  date: string,
-  afterWakingUpMinutes: number
-}> = [
-    {
-      date: '11.06.2022',
-      afterWakingUpMinutes: 50,
-    },
-    {
-      date: '12.06.2022',
-      afterWakingUpMinutes: 30,
-    },
-    {
-      date: '13.06.2022',
-      afterWakingUpMinutes: 40,
-    },
-    {
-      date: '14.06.2022',
-      afterWakingUpMinutes: 30,
-    },
-    {
-      date: '15.06.2022',
-      afterWakingUpMinutes: 20,
-    },
-    {
-      date: '16.06.2022',
-      afterWakingUpMinutes: 5,
-    },
-    {
-      date: '17.06.2022',
-      afterWakingUpMinutes: 0,
-    },
-    {
-      date: 'средняя',
-      afterWakingUpMinutes: (50 + 30 + 40 + 30 + 20 + 5 + 0) / 7,
-    },
-  ]
+const layout = {
+  width: '80%',
+  height: 200
+}
 
 /**
  * время в постели от пробуждения до подъема 
@@ -47,25 +28,118 @@ const data: Array<{
  * @returns 
  */
 const TimeSpentInBedAfterWakingUp: React.FC = () => {
-  const layout = {
-    width: '80%',
-    height: 200
+  /** проснулся */
+  const [
+    awakesTimeData,
+    setAwakesTimeData
+  ] = React.useState<answer[]>([]);
+
+  /** встал с кровати */
+  const [
+    goOutOfBedData,
+    setGoOutOfBedData
+  ] = React.useState<answer[]>([]);
+
+  const { currentUser, selectedUser, startDate, endDate } = useStore()
+
+  const { isFailed, isLoad, onStart, onFailed, onSussess } = useLoading();
+
+
+  const fetchData = (userId: string) => {
+    onStart();
+    http.post(userId, 'question_11', startDate, endDate)
+      .then(withoutDuplicates)
+      .then(setAwakesTimeData)
+      .catch(onFailed)
+    http.post(userId, 'question_12', startDate, endDate)
+      .then(withoutDuplicates)
+      .then(setGoOutOfBedData)
+      .then(onSussess)
+      .catch(onFailed)
   }
+
+  React.useEffect(() => {
+    if (isDoctorPage()) {
+      if (selectedUser) fetchData(selectedUser)
+    } else {
+      if (currentUser) fetchData(currentUser)
+    }
+    // eslint-disable-next-line
+  }, [startDate, endDate, currentUser, selectedUser])
+
+  let data: answer[] = [];
+  // тут считаем разницу между временем пробуждения 
+  // и временем вставания с пастели, 
+  // для каждого просыпания должен быть ответ вставания с постели 
+  awakesTimeData.forEach((awakesTimeAnswer) => {
+    // в диапазоне ответов ищем тот ответ, который был сделан 
+    // в тот же день 
+    // (если есть дубликаты в один день, то берется только последнее значение по времени) 
+    const goOutItemExists = findOneOrFindLast(
+      goOutOfBedData,
+      (goOut) => moment(goOut.cdate).isSame(moment(awakesTimeAnswer.cdate), 'day')
+    )
+    // если нашлось совпадение
+    if (goOutItemExists) {
+      // то валидируем их
+      const isValidDates = validateHHMMstr(awakesTimeAnswer.answer_text)
+        && validateHHMMstr(goOutItemExists.answer_text)
+
+      // если ответы валидны
+      if (isValidDates) {
+        // считаем разницу
+        const goOutH = goOutItemExists.answer_text.split(':')[0]; // левая часть - часы
+        const goOutM = goOutItemExists.answer_text.split(':')[1]; // правая часть - минутки
+
+        const awakeH = awakesTimeAnswer.answer_text.split(':')[0];
+        const awakeM = awakesTimeAnswer.answer_text.split(':')[1];
+
+        // разница в минутках
+        let differenceInMins = (Number(goOutH) * 60 + Number(goOutM)) - (Number(awakeH) * 60 + Number(awakeM));
+        if (differenceInMins < 0) differenceInMins = 0
+        const differenceM = differenceInMins % 60; // остаток деления в минутках
+        const differenceH = Math.floor(differenceInMins / 60); // целое в часах
+        data.push({
+          answer_text: [differenceH, differenceM].join(':'),
+          cdate: awakesTimeAnswer.cdate
+        })
+      }
+    }
+  })
+  //@ts-ignore
+  data = WithAverageInt(strTimeToNum(prepareDate(data)))
+
+  /** Недостаточно данных */
+  const isNotEnought = !data.length;
   return (
     <div className='responsiveChart'>
       <h3>Время нахождения в постели после пробуждения</h3>
+      {isFailed
+        ? <FailedRequest />
+        : isLoad
+          ? <LoaderChart isLoad={isLoad} />
+          : isNotEnought
+            ? <NotEnoughtData />
+            : null
+      }
       <ResponsiveContainer {...layout}>
         <BarChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis dataKey='afterWakingUpMinutes' />
+          <XAxis dataKey="cdate" />
+          <YAxis dataKey='answer_text' />
           <Tooltip />
           <Legend />
           <Bar
             name='Время нахождения в постели после пробуждения'
-            dataKey="afterWakingUpMinutes"
+            dataKey="answer_text"
             fill="#4F81BD"
-          />
+          >
+            {
+              data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.cdate === 'Средняя' ? '#4FBDA3' : '#4F81BD'} />
+              ))
+            }
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     </div>
