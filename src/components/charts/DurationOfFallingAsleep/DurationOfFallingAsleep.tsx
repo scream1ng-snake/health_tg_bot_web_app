1809b48d88260a881a030fcaa1344b2201ee3301
lykasov_am <lykasov_am@lexema.ru>
2023-07-05 16:29:28 +0500

    UPD: duration of falling asleep chart complete

--- src/components/charts/DurationOfFallingAsleep/DurationOfFallingAsleep.tsx
+++ src/components/charts/DurationOfFallingAsleep/DurationOfFallingAsleep.tsx
@@ -1,37 +1,14 @@
 import React from 'react';
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, ResponsiveContainer } from 'recharts';
+import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, ResponsiveContainer, Cell } from 'recharts';
+import { useLoading, useStore } from '../../../hooks';
+import { FailedRequest, http, isDoctorPage, LoaderChart, NotEnoughtData, prepareDate, WithAverage, withoutDuplicates } from '../../common';
 import './DurationOfFallingAsleep.css';
 
+const layout = {
+  width: '80%',
+  height: 200
+}
 
-const data: Array<{
-  date: string,
-  fallingSleepMinutes: number
-}> = [
-    {
-      date: '11.06.2022',
-      fallingSleepMinutes: 60,
-    },
-    {
-      date: '12.06.2022',
-      fallingSleepMinutes: 40,
-    },
-    {
-      date: '13.06.2022',
-      fallingSleepMinutes: 70,
-    },
-    {
-      date: '14.06.2022',
-      fallingSleepMinutes: 100,
-    },
-    {
-      date: '15.06.2022',
-      fallingSleepMinutes: 0,
-    },
-    {
-      date: 'средняя',
-      fallingSleepMinutes: (0 + 60 + 40 + 70 + 100) / 5,
-    },
-  ]
 
 /**
  * время, потраченное на засыпание в минутах за 1 день, 
@@ -40,25 +17,63 @@ const data: Array<{
  * @returns 
  */
 const DurationOfFallingAsleep: React.FC = () => {
-  const layout = {
-    width: '80%',
-    height: 200
+  const [data, setData] = React.useState<answer[]>([]);
+
+  const { currentUser, selectedUser, startDate, endDate } = useStore();
+
+  const { isFailed, isLoad, onStart, onFailed, onSussess } = useLoading();
+
+  /** Недостаточно данных */
+  const isNotEnought = !data.length;
+
+  const fetchData = (userId: string) => {
+    onStart();
+    http.post(userId, 'question_8', startDate, endDate)
+      .then(withoutDuplicates)
+      .then(prepareDate)
+      .then(WithAverage)
+      .then(setData)
+      .then(onSussess)
+      .catch(onFailed)
   }
+
+  React.useEffect(() => {
+    if (isDoctorPage()) {
+      if (selectedUser) fetchData(selectedUser)
+    } else {
+      if (currentUser) fetchData(currentUser)
+    }
+    // eslint-disable-next-line
+  }, [startDate, endDate, currentUser, selectedUser])
   return (
     <div className='responsiveChart'>
-      <h3>время, потраченное на засыпание в минутах за 1 день</h3>
+      <h3>Время, потраченное на засыпание в минутах за 1 день</h3>
+      {isFailed
+        ? <FailedRequest />
+        : isLoad
+          ? <LoaderChart isLoad={isLoad} />
+          : isNotEnought
+            ? <NotEnoughtData />
+            : null
+      }
       <ResponsiveContainer {...layout}>
         <BarChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis dataKey='fallingSleepMinutes' />
+          <XAxis dataKey="cdate" />
+          <YAxis dataKey='answer_text' />
           <Tooltip />
           <Legend />
-          <Bar 
-            name='время, потраченное на засыпание в минутах' 
-            dataKey="fallingSleepMinutes" 
-            fill="#4F81BD" 
-          />
+          <Bar
+            name='Время, потраченное на засыпание в минутах'
+            dataKey="answer_text"
+            fill="#4F81BD"
+          >
+            {
+              data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.cdate === 'Средняя' ? '#4FBDA3' : '#4F81BD'} />
+              ))
+            }
+          </Bar>
         </BarChart>
       </ResponsiveContainer>
     </div>
