b9a38f22f6ef1485315e836854529cf3b4778e41
lykasov_am <lykasov_am@lexema.ru>
2023-07-05 13:23:32 +0500

    UPD: taking sleeping pills chart complete

--- src/components/charts/TakingSleepingPills/TakingSleepingPills.tsx
+++ src/components/charts/TakingSleepingPills/TakingSleepingPills.tsx
@@ -2,25 +2,76 @@ import React from 'react';
 import './TakingSleepingPills.css';
 import { PieChart, Pie, Legend, Tooltip, Cell, ResponsiveContainer } from "recharts";
 import { renderCustomizedLabel } from '../helpers';
+import { useLoading, useStore } from '../../../hooks';
+import { FailedRequest, http, isDoctorPage, LoaderChart, NotEnoughtData, prepareDate } from '../../common';
 
-
-const data02 = [
-  { name: "дней с приемом снотворных", value: 5, color: '#8884d8' },
-  { name: "дней без приема снотворных", value: 2, color: '#82ca9d' },
-];
+const layout = {
+  width: '80%',
+  height: 300
+}
 
 /**
  * число дней с приемом снотворных за неделю (абсолютное значение) 
  * @returns 
  */
 const TakingSleepingPills: React.FC = () => {
-  const layout = {
-    width: '80%',
-    height: 300
+  const [data, setData] = React.useState<answer[]>([]);
+
+  const { currentUser, selectedUser, startDate, endDate } = useStore()
+
+  const { isFailed, isLoad, onStart, onFailed, onSussess } = useLoading();
+
+  /** Недостаточно данных */
+  const isNotEnought = !data.length;
+
+  const fetchData = (userId: string) => {
+    onStart();
+    http.post(userId, 'question_5', startDate, endDate)
+      .then(prepareDate)
+      .then(setData)
+      .then(onSussess)
+      .catch(onFailed)
   }
+
+  React.useEffect(() => {
+    if (isDoctorPage()) {
+      if (selectedUser) fetchData(selectedUser)
+    } else {
+      if (currentUser) fetchData(currentUser)
+    }
+    // eslint-disable-next-line
+  }, [startDate, endDate, currentUser, selectedUser])
+
+  const daysWithPills = data.reduce((acc, cur) =>
+    cur.answer_text === 'true'
+      ? acc+=1
+      : acc+=0,
+    0
+  )
+  const data02 = [
+    { 
+      name: "Дней с приемом снотворных", 
+      value: daysWithPills, 
+      color: '#8884d8' 
+    },
+    { 
+      name: "Дней без приема снотворных", 
+      value: data.length - daysWithPills, 
+      color: '#82ca9d' 
+    },
+  ];
+
   return (
     <div className='responsiveChart'>
       <h3>число дней с приемом снотворных за неделю (абсолютное значение) </h3>
+      {isFailed
+        ? <FailedRequest />
+        : isLoad
+          ? <LoaderChart isLoad={isLoad} />
+          : isNotEnought
+            ? <NotEnoughtData />
+            : null
+      }
       <ResponsiveContainer {...layout}>
         <PieChart width={1000} height={400}>
           <Pie
